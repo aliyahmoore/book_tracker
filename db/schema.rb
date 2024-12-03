72b53ab3f6cc85342765c996141ba7c501c6bcc7
Aliyah Moore <aliyahm.moore@yahoo.com>
2024-12-03 14:07:34 -0600

    generated and migrated notes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_03_190703) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_03_200036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_03_190703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_club_id"], name: "index_books_on_book_club_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.integer "book_club_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_club_id"], name: "index_notes_on_book_club_id"
+    t.index ["book_id"], name: "index_notes_on_book_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
